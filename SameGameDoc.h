f9779d59edfc683a3299a7c3453221b0e2dd1f2c
George Makarichev <geo.m2019@mail.ru>
2023-02-25 07:23:50 +0300

    Добавьте файлы проекта.

---
+++ SameGameDoc.h
@@ -0,0 +1,48 @@
+﻿
+// SameGameDoc.h: интерфейс класса CSameGameDoc 
+//
+
+
+#pragma once
+
+
+class CSameGameDoc : public CDocument
+{
+protected: // создать только из сериализации
+	CSameGameDoc() noexcept;
+	DECLARE_DYNCREATE(CSameGameDoc)
+
+// Атрибуты
+public:
+
+// Операции
+public:
+
+// Переопределение
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// Реализация
+public:
+	virtual ~CSameGameDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Созданные функции схемы сообщений
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// Вспомогательная функция, задающая содержимое поиска для обработчика поиска
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
---
+++ SameGameView.cpp
@@ -0,0 +1,82 @@
+﻿
+// SameGameView.cpp: реализация класса CSameGameView
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "SameGame.h"
+#endif
+
+#include "SameGameDoc.h"
+#include "SameGameView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSameGameView
+
+IMPLEMENT_DYNCREATE(CSameGameView, CView)
+
+BEGIN_MESSAGE_MAP(CSameGameView, CView)
+END_MESSAGE_MAP()
+
+// Создание или уничтожение CSameGameView
+
+CSameGameView::CSameGameView() noexcept
+{
+	// TODO: добавьте код создания
+
+}
+
+CSameGameView::~CSameGameView()
+{
+}
+
+BOOL CSameGameView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: изменить класс Window или стили посредством изменения
+	//  CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Рисование CSameGameView
+
+void CSameGameView::OnDraw(CDC* /*pDC*/)
+{
+	CSameGameDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: добавьте здесь код отрисовки для собственных данных
+}
+
+
+// Диагностика CSameGameView
+
+#ifdef _DEBUG
+void CSameGameView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSameGameView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSameGameDoc* CSameGameView::GetDocument() const // встроена неотлаженная версия
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSameGameDoc)));
+	return (CSameGameDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Обработчики сообщений CSameGameView
